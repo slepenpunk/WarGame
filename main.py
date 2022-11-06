c03e9ed6f8fba09e150f283f552510e95b7463af
slepenpunk <slepenko.m@mail.ru>
2022-11-06 22:23:35 +0300

    hz

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# Create cats with different specifications of color, mood and age.
+# Create cats with different specifications of name, color, mood and age.
 import random
 
 
 class Cats():
     """Create different cats"""
+    cats_dict = {}
     _total = 0
+    names = ('Choma', 'Kevin', 'Bastard', 'Kitty')
     colors = ['White', 'Black', 'Gray', 'Brown']
     moods = ('凸(￣ヘ￣)', '٩(ఠ益ఠ)۶', '(▽◕ ᴥ ◕▽)', '(´｡• ᵕ •｡)')
 
@@ -17,28 +19,39 @@ class Cats():
         new = Cats.colors.append(color)
         return new
 
-    # @classmethod
-    # def random_cat(cls):
-    #     color = random.choice(cls.colors)
-    #     mood = random.choice(cls.moods)
-    #     age = random.randint(1, 20)
-    #     return cls(color, mood, age)
+    @classmethod
+    def random_cat(cls):
+        color = random.choice(cls.colors)
+        mood = random.choice(cls.moods)
+        age = random.randint(1, 20)
+        name = random.choice(cls.names)
+        return cls(color, mood, age, name)
 
     @staticmethod
-    def random_cat():
-        c = random.choice(Cats.colors)
-        m = random.choice(Cats.moods)
-        a = random.randint(1, 20)
-        return Cats(c, m, a)
+    def find_cat_by_name():
+        c_dict = Cats.cats_dict
+        find_name = input('Enter name to find a cat: ').capitalize()
+        if find_name in c_dict:
+            for i in c_dict.values():
+                print(f'{find_name}\n'
+                      f'Color - {i[0]}\n'
+                      f'Age - {i[1]}\n'
+                      f'Mood - {i[2]}\n')
+        else:
+            print('There is no such cat!')
 
-    def __init__(self, color, mood, age):
+    def __init__(self, color, mood, age, name):
         self.color = color
         self.mood = mood
         self.age = age
+        self.name = name
+        c_dict = Cats.cats_dict
+        c_dict[name] = color, age, mood
         Cats._total += 1
 
     def __str__(self):
         rep = (f'\nA new cat has been created!\n'
+               f'Name - {self.name}\n'
                f'Color - {self.color}\n'
                f'Mood - {self.mood}\n'
                f'Age - {self.age}')
@@ -57,6 +70,7 @@ def main():
             3 - View the colors of cats
             4 - Add color
             5 - Delete color
+            6 - Find a cat by name
             ''')
         choice = input('Enter your choice: ')
         if choice == '0':
@@ -64,11 +78,14 @@ def main():
         elif choice == '1':
             try:
                 c = int(input('Enter how many cats you want to create: '))
-                if colors:
-                    for i in range(c):
-                        print(Cats.random_cat())
+                if c <= 20:
+                    if colors:
+                        for i in range(c):
+                            print(Cats.random_cat())
+                    else:
+                        print('No colors!')
                 else:
-                    print('No colors!')
+                    print('Max 20 cats at a time!')
             except ValueError:
                 print('Enter only numbers!')
         elif choice == '2':
@@ -87,6 +104,8 @@ def main():
                 print('Color', delete_color, 'was been deleted!')
             else:
                 print('No such is color!')
+        elif choice == '6':
+            Cats.find_cat_by_name()
         else:
             print('Unknown command!')
 
